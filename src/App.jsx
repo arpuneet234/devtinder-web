ebd7d020eecf99eee2809c5ee930766bea84adcd
Puneet Arora <arorapuneet234@gmail.com>
2026-02-16 12:46:15 +0530

    Added react-router-dom created some component and routes for them

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,19 @@
 import React from "react";
 import NavBar from "./NavBar";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Body from "./Body";
 
 function App() {
-  return <NavBar />;
+  return (
+    <BrowserRouter basename="/">
+      <Routes>
+        <Route path="/" element={<Body />}>
+          <Route path="/login" element={<div>Login Page</div>} />
+          <Route path="/test" element={<div>Test Page</div>} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
